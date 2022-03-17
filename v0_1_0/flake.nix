fd67e58ae0d84b7470adb4e17c561d7625fcbfbe
hugosenari <hugosenari@gmail.com>
2022-03-17 18:24:43 -0300

    chore: re-index ../nimpkgs/c/cbor/v0_1_0

--- v0_1_0/flake.nix
+++ v0_1_0/flake.nix
@@ -11,7 +11,7 @@
   inputs.src-cbor-v0_1_0.owner = "~ehmry";
   inputs.src-cbor-v0_1_0.ref   = "refs/tags/v0.1.0";
   inputs.src-cbor-v0_1_0.repo  = "nim_cbor";
-  inputs.src-cbor-v0_1_0.type  = "other";
+  inputs.src-cbor-v0_1_0.type  = "sourcehut";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
