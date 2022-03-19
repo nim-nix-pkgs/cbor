83eee96d9c099aec896206dfdc4a6d62337d9ee1
hugosenari <hugosenari@gmail.com>
2022-03-19 03:55:39 -0300

    chore: re-index ../nimpkgs/c/cbor/master

--- master/flake.nix
+++ master/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-cbor-master.flake = false;
   inputs.src-cbor-master.owner = "~ehmry";
-  inputs.src-cbor-master.ref   = "refs/heads/master";
+  inputs.src-cbor-master.ref   = "master";
   inputs.src-cbor-master.repo  = "nim_cbor";
   inputs.src-cbor-master.type  = "sourcehut";
   
