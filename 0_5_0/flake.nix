654622bb08784e62c1c429c70bd84eda076ea6f1
hugosenari <hugosenari@gmail.com>
2022-03-19 03:55:43 -0300

    chore: re-index ../nimpkgs/c/cbor/0_5_0

--- 0_5_0/flake.nix
+++ 0_5_0/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-cbor-0_5_0.flake = false;
   inputs.src-cbor-0_5_0.owner = "~ehmry";
-  inputs.src-cbor-0_5_0.ref   = "refs/tags/0.5.0";
+  inputs.src-cbor-0_5_0.ref   = "0.5.0";
   inputs.src-cbor-0_5_0.repo  = "nim_cbor";
   inputs.src-cbor-0_5_0.type  = "sourcehut";
   
